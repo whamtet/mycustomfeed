d950ce2c264e464b13ac0abd6238bdd846d4e903
Matthew Molloy <whamtet@gmail.com>
2025-04-10 12:40:03 +0900

    tried everything and wrote notes

--- extension/dist/service-worker.js
+++ extension/dist/service-worker.js
@@ -3,6 +3,10 @@
 chrome.runtime.onMessage.addListener((message, sender) => {
     // The callback for runtime.onMessage must return falsy if we're not sending a response
     (async () => {
+        // closing the sidePanel breaks the extension icon - don't do it!
+        // you can reopen the sidePanel
+        // however getOptions will always return enabled: true regardless of whether the tab is open
+        // AGGGHHHH!!!
         if (message.type === 'open_side_panel') {
             // This will open a tab-specific side panel only on the current tab.
             await chrome.sidePanel.open({ tabId: sender.tab.id });
@@ -14,7 +18,6 @@ const HOST = 'www.linkedin.com';
 
 chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
     const url = new URL(tab.url);
-    // Enables the side panel on google.com
     if (url.host === HOST) {
         await chrome.sidePanel.setOptions({
             tabId,
