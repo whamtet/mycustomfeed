f097e493194c069bc053c330083d7f4481399610
Matthew Molloy <whamtet@gmail.com>
2025-04-16 10:57:51 +0900

    icons

--- extension/src/sidepanel.js
+++ extension/src/sidepanel.js
@@ -3,5 +3,3 @@ const BASE_URL = process.env.BASE_URL;
 htmx.config.defaultSwapStyle = 'outerHTML';
 
 htmx.ajax('GET', BASE_URL + '/extension/login?count=0', '#container')
-
-console.log('BASE_URL', BASE_URL);
