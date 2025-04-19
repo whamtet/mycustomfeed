feeddd2ae687950629d3b939e19965cf5c74caa1
matt <matt@localhost.my.domain>
2025-04-19 14:54:19 +0900

    wip

--- extension/src/main.js
+++ extension/src/main.js
@@ -1,12 +1,12 @@
 import { GET } from "./client.js";
 
-const clickButton = () => {
+const clickSidePanelButton = () => {
     // the sidePanel API is too broken to do anything other than open.
     // in theory we could ping sidepanel.html, but let's keep things simple for now
     chrome.runtime.sendMessage({ type: 'open_side_panel' });
 }
 
-(async () => {
+const addSidePanelButton = async () => {
     const response = await GET('/api/button');
     const html = await response.text();
 
@@ -14,5 +14,10 @@ const clickButton = () => {
     div.innerHTML = html;
     document.body.append(div);
 
-    div.addEventListener('click', clickButton);
-})()
+    div.addEventListener('click', clickSidePanelButton);
+}
+addSidePanelButton();
+
+const notifyProfilePage = () => {
+    // document.body.innerHTML.match(/urn:li:fsd_profile:([a-zA-Z0-9]+)/g);
+}
